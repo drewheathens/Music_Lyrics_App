64d28c54854a2a359f088b02befccfcb6215d17f
raeman21 <mandelar94@gmail.com>
2019-02-20 10:43:21 +0300

    add User and Role model create requirements file

---
+++ manage.py
@@ -0,0 +1,10 @@
+from app.models import User,Role
+
+
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Role = Role )
+if __name__ == '__main__':
+    manager.run()
