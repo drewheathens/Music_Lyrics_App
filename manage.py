b939b30f543ebc027ee11446279392ad6282125b
raeman21 <mandelar94@gmail.com>
2019-02-20 11:58:46 +0300

    update manage.py to have only shell command

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-import os
 from app import create_app, db
 from flask_script import Manager,Server
 from app.models import User,Role
@@ -7,22 +6,10 @@ from  flask_migrate import Migrate, MigrateCommand
 #creating app
 app = create_app('production')
 
-manager = Manager(app)
-manager.add_command("server",Server)
+
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
-
-@manager.command
-def test():
-    """
-    Run the unit tests
-    """
-    import unittest
-    tests = unittest.TestLoader().discover("tests")
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-
 
 @manager.shell
 def make_shell_context():
