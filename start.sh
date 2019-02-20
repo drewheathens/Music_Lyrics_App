97e9fa2fa449ec0946c6cfe9df41fbb4205a1864
raeman21 <mandelar94@gmail.com>
2019-02-20 10:50:49 +0300

    add secret key mail uname and pword to start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY='1234'
+export MAIL_USERNAME='mandelar94@gmail.com'
+export MAIL_PASSWORD='Yaluchipyervino1.me'
+export FLASK_APP="app.py"
+export FLASK_DEBUG=1
+
+python3.6 manage.py server
